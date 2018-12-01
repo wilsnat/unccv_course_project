647cfbe4a27ccba25d4d8dbac53e56bdd02d62a9
wilsnat <wilsnat89@gmail.com>
2018-11-30 19:20:29 -0500

    changed for modularity and comparing new dataset

--- nates_regression_mean_center_pixel.py
+++ nates_regression_mean_center_pixel.py
@@ -8,60 +8,70 @@ import matplotlib.pyplot as plt
 import pdb;
 from data_loader import data_loader
 
-data = data_loader()
+def main():
+    data = data_loader()
+    data = data_prep(data)
+    print("train of shape: " + str(data.train.im.shape) + " test of shape: " + str(data.test.im.shape))
+    model = build_model(data)
+    model.summary()
 
-m = data.train.ex.max(axis=0)
-data.train.ex = (data.train.ex)/m
-data.test.ex = (data.test.ex)/m
-maxhue = 360
-data.train.im[:,:,:,0] = (data.train.im[:,:,:,0])/maxhue
-data.test.im[:,:,:,0] = (data.test.im[:,:,:,0])/maxhue
-data.train.y[:,0] = data.train.y[:,0]/maxhue
-data.test.y[:,0] = data.test.y[:,0]/maxhue
+    EPOCHS = 1500
 
-h,w,d = data.train.im[0].shape
-data.train.immean = data.train.im.mean(axis=(1,2))
-data.test.immean = data.test.im.mean(axis=(1,2))
-pdb.set_trace()
-data.train.im = data.train.im[:,round(h/2),round(w/2),:]
-data.test.im = data.test.im[:,round(h/2),round(w/2),:]
+    # Store training stats
+    history = model.fit(data.train.full, data.train.y, epochs=EPOCHS,
+                        verbose=0,
+                        callbacks=[PrintDot()])
+    plot_history(history)
 
-data.train.full = np.concatenate((data.train.im,data.train.immean,data.train.ex), axis=1)
-data.test.full = np.concatenate((data.test.im,data.test.immean,data.test.ex), axis=1)
+    data2 = data_loader(train_test_split = 0.04, set = "set02")
+    data2 = data_prep(data2)
 
-print("train of shape: " + str(data.train.im.shape) + " test of shape: " + str(data.test.im.shape))
+    #predict(model,data)
+    predict(model,data2)
 
-def build_model():
-  model = keras.Sequential([
+def data_prep(data):
+    #m = np.concatenate((data.train.ex,  data.test.ex),axis=0).max(axis=0)
+    m=2400
+    data.train.ex = (data.train.ex)/m
+    data.test.ex = (data.test.ex)/m
+    maxhue = 360
+    data.train.im[:,:,:,0] = (data.train.im[:,:,:,0])/maxhue
+    data.test.im[:,:,:,0] = (data.test.im[:,:,:,0])/maxhue
+    data.train.y[:,0] = data.train.y[:,0]/maxhue
+    data.test.y[:,0] = data.test.y[:,0]/maxhue
+
+    h,w,d = data.train.im[0].shape
+    data.train.immean = data.train.im.mean(axis=(1,2))
+    data.test.immean = data.test.im.mean(axis=(1,2))
+    cH = round(h/2)
+    cW = round(w/2)
+    data.train.im = data.train.im[:,cH-3:cH+3,cW-3:cW+3,:].mean(axis=(1,2))
+    data.test.im = data.test.im[:,cH-3:cH+3,cW-3:cW+3,:].mean(axis=(1,2))
+
+    data.train.full = np.concatenate((data.train.im,data.train.immean,data.train.ex), axis=1)
+    data.test.full = np.concatenate((data.test.im,data.test.immean,data.test.ex), axis=1)
+    return data
+
+def build_model(data):
+    print(data.train.full.shape[1])
+    model = keras.Sequential([
     keras.layers.Dense(64, activation=tf.nn.relu,
                        input_shape=(data.train.full.shape[1],)),
     keras.layers.Dense(64, activation=tf.nn.relu),
     keras.layers.Dense(3)
-  ])
+    ])
+    optimizer = tf.train.RMSPropOptimizer(0.001)
 
-  optimizer = tf.train.RMSPropOptimizer(0.001)
-
-  model.compile(loss='mse',
+    model.compile(loss='mse',
                 optimizer=optimizer,
                 metrics=['mae'])
-  return model
-
-model = build_model()
-model.summary()
-
+    return model
 
 # Display training progress by printing a single dot for each completed epoch
 class PrintDot(keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs):
     if epoch % 100 == 0: print('')
     print('.', end='')
-
-EPOCHS = 1500
-
-# Store training stats
-history = model.fit(data.train.full, data.train.y, epochs=EPOCHS,
-                    verbose=0,
-                    callbacks=[PrintDot()])
 
 def plot_history(history):
   plt.figure()
@@ -72,8 +82,9 @@ def plot_history(history):
   plt.legend()
   plt.show()
 
-def predict(test_data = data.test.full):
-    test_predictions = model.predict(test_data).flatten()
+def predict(model, data):
+    pdb.set_trace()
+    test_predictions = model.predict(data.test.full).flatten()
 
     plt.title('data')
     plt.scatter(data.test.y.flatten(), test_predictions)
@@ -85,5 +96,4 @@ def predict(test_data = data.test.full):
     _ = plt.plot([-100, 100], [-100, 100])
     plt.show()
 
-plot_history(history)
-predict()
+main()
