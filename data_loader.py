e1fa467ffa86de1bf66a0fa4fd50b08037cd47b9
shweta <shweta.mangle@wwindia.com>
2018-12-04 15:34:09 -0500

    k means algorithm for extracting dominant color and color name detector

--- data_loader.py
+++ data_loader.py
@@ -67,9 +67,9 @@ def data_loader(train_test_split = 0.7,
 		data.train.ex[count] = exif[index]
 		data.train.y[count] = hls[math.floor(index/lab_samples_per_color)]
 
-		print(im_paths[index])
+		'''print(im_paths[index])
 		print(data.train.y[count])
-		print('------')
+		print('------')'''
 
 	for count, index in enumerate(testing_indices):
 		imin =  np.float32(cv2.imread(im_paths[index]))/255
